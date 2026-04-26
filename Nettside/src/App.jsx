5c4c4a3e45cd6e216dd1b94ac9b510667daaf129
KevinsLarsen <kevin.s.larsen04@gmail.com>
2026-04-26 14:17:31 +0200

    Fikset ferdig KevinProfile men mangler bildet

--- Nettside/src/App.jsx
+++ Nettside/src/App.jsx
@@ -1,27 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import ArbeidskravListe from "./components/ArbeidskravListe";
+import KevinProfile from "./components/profiles/KevinProfile";
 import "./styles/App.scss";
 
 function App() {
   const [arbeidskrav, setArbeidskrav] = useState([]);
 
-
   return (
     <>
-      <Header gruppenummer="31"/>
+      <Header gruppenummer="31" />
+
       <main>
         <section>
-
+          <h2>Gruppemedlemmer</h2>
+          <KevinProfile />
         </section>
 
         <section>
           <h2>Arbeidskrav i UIN</h2>
-          <ArbeidskravListe krav={arbeidskrav}/>
+          <ArbeidskravListe krav={arbeidskrav} />
         </section>
       </main>
     </>
-  )
+  );
 }
 
 export default App;
