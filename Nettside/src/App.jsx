20acc997c979a51b30085f34365ba2867eb824f9
Muharrem <muhmer11@hotmail.com>
2026-04-27 00:08:41 +0200

    muharrem profil component

--- Nettside/src/App.jsx
+++ Nettside/src/App.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import ArbeidskravListe from "./components/ArbeidskravListe";
+import MuharremProfil from "./components/MuharremProfil";
 import "./styles/App.scss";
 
 function App() {
   const [arbeidskrav, setArbeidskrav] = useState([]);
 
-
   return (
     <>
       <Header gruppenummer="31"/>
+
       <main>
         <section>
+          <h2>Gruppemedlemmer</h2>
 
+          <ul>
+            <MuharremProfil />
+          </ul>
         </section>
 
         <section>
---
+++ Nettside/src/components/MuharremProfil.jsx
@@ -0,0 +1,16 @@
+export default function MuharremProfil() {
+  return (
+    <li>
+      <article className="muharremProfil">
+        <img src="/src/assets/hero.png" alt="profil bilde" />
+
+        <h3>Muharrem Merovci</h3>
+
+        <p>Student ved Høgskolen i Østfold</p>
+        <p>Bachelor i digitale medier og design</p>
+
+        <a href="muharrem@hiof.no">muharrem@hiof.no</a>
+      </article>
+    </li>
+  )
+}
