86313e58f087b7484f82d25ec90f14cc91c87f3a
AdrianNebelung <acnebelu@hiof.no>
2026-04-27 10:45:37 +0200

    .

--- Nettside/src/App.jsx
+++ Nettside/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import ArbeidskravListe from "./components/ArbeidskravListe";
+import Adriankort from "./components/Adrian/Adriankort";
 import "./styles/App.scss";
 
 function App() {
