4934b24eef5ef76bc97c18503d624a838bc16965
AdrianNebelung <acnebelu@hiof.no>
2026-04-27 11:35:45 +0200

    Gjort noen små endringer

--- Nettside/src/App.jsx
+++ Nettside/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import ArbeidskravListe from "./components/ArbeidskravListe";
-<<<<<<< HEAD
 import Adriankort from "./components/Adrian/Adriankort";
-=======
 import KevinProfile from "./components/profiles/KevinProfile";
->>>>>>> origin/kevin-profile
 import "./styles/App.scss";
 
 function App() {
@@ -17,21 +14,12 @@ function App() {
 
       <main>
         <section>
-<<<<<<< HEAD
           <Adriankort/>
-        </section>
-
-        <section>
-          <ArbeidskravListe krav={arbeidskrav}/>
-=======
-          <h2>Gruppemedlemmer</h2>
           <KevinProfile />
         </section>
 
         <section>
-          <h2>Arbeidskrav i UIN</h2>
-          <ArbeidskravListe krav={arbeidskrav} />
->>>>>>> origin/kevin-profile
+          <ArbeidskravListe krav={arbeidskrav}/>
         </section>
       </main>
     </>
