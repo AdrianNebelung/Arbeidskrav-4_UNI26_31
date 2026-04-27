39df02b952e8af0f88e10965fb5d10cfa6effb64
AdrianNebelung <acnebelu@hiof.no>
2026-04-27 10:52:16 +0200

    ..

--- Nettside/src/App.jsx
+++ Nettside/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header gruppenummer="31"/>
       <main>
         <section>
-
+          <Adriankort/>
         </section>
 
         <section>
--- Nettside/src/components/Adrian/Adriankort.jsx
+++ Nettside/src/components/Adrian/Adriankort.jsx
@@ -1,7 +1,9 @@
+import "./AdrianSass/Adrianscss.scss";
+
 export default function Adriankort() {
     return (
         <article className="kort-adrian">
-            <img src="#"/>
+            <img src="/bilde/Adrian.webp"/>
             <h2>Adrian Cesar Nascimento Nebelung</h2>
             <a href="mailto:acnebelu@hiof.no">acnebelu@hiof.no</a>
             <p>Bachelor i Informasjonssystemer</p>
