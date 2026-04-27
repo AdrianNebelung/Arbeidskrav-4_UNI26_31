82228efaa855dd6d67016b4b28a9818aefe1f0b3
AdrianNebelung <acnebelu@hiof.no>
2026-04-27 11:31:43 +0200

    ..

--- Nettside/src/components/Adrian/Adriankort.jsx
+++ Nettside/src/components/Adrian/Adriankort.jsx
@@ -5,9 +5,6 @@ import "./AdrianSass/Adrianscss.scss";
 export default function Adriankort() {
     return (
         <article className="kort-adrian">
-
-            <img src="*"/>
-
             <img src="/bilde/Adrian.webp"/>
             <h2>Adrian Cesar Nascimento Nebelung</h2>
             <a href="mailto:acnebelu@hiof.no">acnebelu@hiof.no</a>
